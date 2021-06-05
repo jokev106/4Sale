56f2c64d26efd029ecbd7dbd29e7f721a414b059
jokev106 <kevinmarsiano@gmail.com>
2021-06-05 23:47:42 +0700

    container kosong

--- forsale/lib/ui/pages/adddata.dart
+++ forsale/lib/ui/pages/adddata.dart
@@ -1,1 +1,18 @@
 part of 'pages.dart';
+
+class AddData extends StatefulWidget {
+
+  @override
+  _AddDataState createState() => _AddDataState();
+}
+
+class _AddDataState extends State<AddData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Data'),
+      ),
+    );
+  }
+}
--- forsale/lib/ui/pages/myacc.dart
+++ forsale/lib/ui/pages/myacc.dart
@@ -1,1 +1,18 @@
 part of 'pages.dart';
+
+class MyAcc extends StatefulWidget {
+
+  @override
+  _MyAccState createState() => _MyAccState();
+}
+
+class _MyAccState extends State<MyAcc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Account'),
+      ),
+    );
+  }
+}
